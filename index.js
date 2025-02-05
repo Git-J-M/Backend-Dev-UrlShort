ea5b42f130967e3d839cd269bb5e14ec28645767
Paarthurax <privatedndemail@gmail.com>
2025-02-05 12:33:22 +0100

    Url Shortener works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const urls = [];
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -22,3 +22,42 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+// For parsing application/json
+app.use(express.json());
+
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/shorturl', (req, res) => {
+  let url_ori = req.body.url;
+  let url_short;
+
+  console.log(url_ori);
+
+  
+  let urlPattern = /^(https?:\/\/)(localhost(:\d+)?|[\w-]+(\.[\w-]+)+)(\/[\w-./?%&=]*)?$/;
+  if (!urlPattern.test(url_ori)) {
+    console.log("invalid")
+    return res.json({ error: 'invalid url' });
+  }
+  else{
+    if(!urls.includes(url_ori )){
+      urls.push(url_ori );
+      url_short = urls.length;
+    }
+    else{
+      url_short = urls.indexOf(url_ori) +  1;
+    }
+    console.log(url_ori);
+    res.json({original_url: url_ori, short_url: url_short});
+    res.send();
+  }
+});
+
+app.get('/api/shorturl/:number', (req, res) => {
+  let number = req.params.number;
+  if(urls.length >= number && number > 0){
+    res.redirect(urls[number - 1]);
+  }
+})
